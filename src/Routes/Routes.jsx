e89e03658bbf56243349344e0bc8d589a0a83051
muradhossain1 <muradssq12@gmail.com>
2024-12-23 10:25:47 +0600

    Home page recent blogs section create fetch data server site

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,7 +17,8 @@ const routes = createBrowserRouter([
         children : [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Home></Home>,
+                loader: () => fetch('http://localhost:5000/recent-blogs')
             },
             {
                 path: '/all-blogs',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../Components/Banner";
+import BlogCrad from "../Components/BlogCrad";
 
 
 
 const Home = () => {
+    const blogs = useLoaderData();
     return (
         <div>
             {/* banner */}
             <div>
                 <Banner></Banner>
             </div>
-            <main></main>
+            <main className="mt-12">
+            <h2 className="text-2xl md:text-4xl text-center font-bold ">Recent Blogs Section</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:px-12 lg:px-32 mt-6">
+                    {
+                        blogs.map(blog => <BlogCrad key={blog._id} blog={blog}></BlogCrad>)
+                    }
+                </div>
+            </main>
         </div>
     );
 };
